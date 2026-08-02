8388d922ad0659f3ab982df2eef041b7ebe1aacc
luizfernando <lf.nandooliveeira@gmail.com>
2026-08-02 18:54:38 -0300

    Feat: Adiciona service com regras de negocio no metodo validarTransacao

--- src/main/java/com/dev/desafiobackenditau/TransacaoService.java
+++ src/main/java/com/dev/desafiobackenditau/TransacaoService.java
@@ -1,4 +1,27 @@
 package com.dev.desafiobackenditau;
 
+import org.jspecify.annotations.NonNull;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+import java.time.OffsetDateTime;
+import java.util.Objects;
+
+@Service
 public class TransacaoService {
+
+    public void validarTransacao(@NonNull TransacaoRequest transacaoRequest) {
+
+        if (transacaoRequest.valor() == null || transacaoRequest.dataHora() == null) {
+            throw new IllegalArgumentException("Erro: Request inválida");
+        }
+
+        if (transacaoRequest.valor().compareTo(BigDecimal.ZERO) < 0) {
+            throw new IllegalArgumentException("Erro: O valor da transação não pode ser negativo");
+        }
+
+        if (transacaoRequest.dataHora().isAfter(OffsetDateTime.now())) {
+            throw new IllegalArgumentException("Erro: A data e hora da transação não podem ser no futuro");
+        }
+    }
 }
