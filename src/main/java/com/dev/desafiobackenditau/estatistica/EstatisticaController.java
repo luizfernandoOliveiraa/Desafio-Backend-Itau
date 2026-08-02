d279a6e995082c89244f86e16b4284859adb8975
luizfernando <lf.nandooliveeira@gmail.com>
2026-08-02 20:00:34 -0300

    Feat: Adiciona controller para rota de estatisticas

---
+++ src/main/java/com/dev/desafiobackenditau/estatistica/EstatisticaController.java
@@ -0,0 +1,11 @@
+package com.dev.desafiobackenditau.estatistica;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/estatistica")
+public class EstatisticaController {
+
+    
+}
