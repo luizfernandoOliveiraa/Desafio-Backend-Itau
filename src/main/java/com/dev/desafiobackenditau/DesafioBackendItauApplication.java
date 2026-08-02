f36746b262256a8f6d4304ab1d5b31f059791e57
luizfernando <lf.nandooliveeira@gmail.com>
2026-08-02 18:59:01 -0300

    Config: Adiciona arquivos de configuracao pom e classe main do programa

--- src/main/java/com/dev/desafiobackenditau/DesafioBackendItauApplication.java
+++ src/main/java/com/dev/desafiobackenditau/DesafioBackendItauApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DesafioBackendItauApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DesafioBackendItauApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DesafioBackendItauApplication.class, args);
+    }
 
 }
